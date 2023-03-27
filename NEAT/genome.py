eb825a05bec3a292fb1082e052ce66c0166b82d9
Drew Curran <denisac@princeton.edu>
2023-03-27 13:23:50 -0400

    Update genome.py

--- NEAT/genome.py
+++ NEAT/genome.py
@@ -7,10 +7,27 @@ class Genome:
         self.inputs = inputs
         self.outputs = outputs
         self.layers = layers
+
+        self.next_node = 0
+        self.nodes = []
+        for i in range(self.inputs):
+            node = Node(i)
+            self.nodes.append(node)
+            node.layer = 0
+            self.next_node += 1
+
+        for o in range(self.outputs):
+            node = Node(o + self.inputs)
+            self.nodes.append(node)
+            node.layer = 1
+            self.next_node += 1
+        
+        self.bias_node = Node(self.next_node)
+        
         self.genes = None
-        self.nodes = None
-        self.next_node = None
-        self.bias_node = None
+        
+        
+        
         self.network = None
 
     # Get node with matching label
@@ -33,7 +50,39 @@ class Genome:
             self.inputs[i].output_value = input_values[i]
         self.nodes[self.bias_node].output_value = 1
 
-        for node in self.nodes:
+        for node in self.network:
             node.get_value()
+        
+        out = []
+        for i in range(self.outputs):
+            out[i] = self.nodes.get(self.inputs + i)
+        
+        for node in self.nodes:
+            node.input_value = 0
+        
+        return out
+    
+    # Set up neural network
+    def generate_network(self):
+        self.connect_nodes()
+        self.network = []
+        for l in range(self.layers):
+            for node in self.nodes:
+                if (node.layer == l):
+                    self.network.append(node)
+
+    # Mutate the genome
+    def mutate(self):
+        if np.random.uniform() < 0.8:
+            for gene in self.genes:
+                gene.mutate_weight()
+
+
+    # Add a new node
+
+    # Add a new connection
+
+
+
         
         
